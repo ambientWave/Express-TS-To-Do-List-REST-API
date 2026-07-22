c3ef88dca17708cbf393fee60b9b1c0407e3e3fa
ambientWave <minaw368@gmail.com>
2026-07-23 01:51:07 +0300

    Stage 1: root and health endpoints

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,12 @@ import express, { type Express, type Request, type Response } from 'express';
 const app: Express = express();
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
+    const appInfo: Object = { "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] };
+    res.send(JSON.stringify(appInfo));
+});
+
+app.get('/health', (req: Request, res: Response) => {
+    res.send(JSON.stringify({ "status": "ok" }));
 });
 
 app.listen(3000);
